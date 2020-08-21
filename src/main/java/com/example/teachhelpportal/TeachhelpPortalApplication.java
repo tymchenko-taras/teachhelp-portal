69790d76edc4ed30c382993b9e6a2c54cb558679
tymchenko-taras <t.teachhelp@gmail.com>
2020-08-20 21:06:04 -0400

    init - new project

---
+++ src/main/java/com/example/teachhelpportal/TeachhelpPortalApplication.java
@@ -0,0 +1,13 @@
+package com.example.teachhelpportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachhelpPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeachhelpPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/teachhelpportal/TeachhelpPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teachhelpportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeachhelpPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
